d6dbf6b5f1c66dcffa36bf9471c20c928cd10f25
Alex <alex@passiolife.com>
2022-06-01 11:43:00 -0400

    new release

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ rm package-lock.json
 npm install
 npm run build
 git tag -d v1
+git commit -am "new release"
 git push origin :refs/tags/v1
+git push origin refs/heads/v1
+gh release create v1 --target refs/heads/v1 -t v1 -n ""
